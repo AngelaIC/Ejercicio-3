0fac916eb688acb0035ba6c558f0722b6c20daba
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-09-21 15:08:43 -0500

    Add files via upload

---
+++ Ejercicio 3/app.js
@@ -0,0 +1,8 @@
+function suma(){
+    var a = parseInt(prompt("Introduce un número"));
+    var b = parseInt(prompt("Ingrese otro número"));
+    var c = parseInt(a) + parseInt(b);
+    alert("la suma de estos números es: " + c); 
+    console.log("la suma de estos números es: " + c);
+}
+
